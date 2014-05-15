9ad96097cd18f5f8de9e996227989635252b2fda
Azhdev <Azhdev@hotmail.com>
2014-05-15 19:11:31 +0200

    commit 8

--- main/java/nl/Azhdev/client/models/modelSpinny.java
+++ main/java/nl/Azhdev/client/models/modelSpinny.java
@@ -19,16 +19,19 @@ public class modelSpinny extends ModelBase{
 		mainTwistPart.setRotationPoint(0, 0, 0);
 		parts.add(mainTwistPart);
 		
-		for(float r = 0; r < Math.PI * 2; r += Math.PI){
-			ModelRenderer side = new ModelRenderer(this, 0, 30);
-			side.addBox(-5, -2.5F, 5, 10, 5, 2);
-			side.setRotationPoint(0, 0, 0);
-			side.rotateAngleY = r;
-			mainTwistPart.addChild(side);
-		}
+		ModelRenderer side1 = new ModelRenderer(this, 0, 30);
+		side1.addBox(-5, -2.5F, 5, 10, 5, 2);
+		side1.setRotationPoint(0, 0, 0);
+		mainTwistPart.addChild(side1);
+		
+		ModelRenderer side2 = new ModelRenderer(this, 0, 37);
+		side2.addBox(-5, -2.5F, 5, 10, 5, 2);
+		side2.rotateAngleY = (float)(Math.PI * 2);
+		side2.setRotationPoint(0, 0, 0);
+		mainTwistPart.addChild(side2);
 		
 		ModelRenderer back = new ModelRenderer(this, 24, 30);
-		back.addBox(-5, -2.5F, 5, 14, 5, 2);
+		back.addBox(-7, -2.5F, 5, 14, 5, 2);
 		back.setRotationPoint(0, 0, 0);
 		back.rotateAngleY = (float)(Math.PI * 1.5F);
 		mainTwistPart.addChild(back);
